63bcb6880b4dc6c3f24e22cb007aeb99f177b841
seo401b <tjtkddnjs880@naver.com>
2026-08-30 10:20:52 +0000

    03_SocketClass

---
+++ 03_SocketClass/testServer.cpp
@@ -0,0 +1,38 @@
+#include "TcpSocket.h"
+#include <iostream>
+#include <cstring>
+
+int main(){
+    TcpSocket server;
+
+    if(!server.Bind(8080) || !server.Listen(5)){
+        return 1;
+    }
+
+    std::cout << "Server Waiting(8080)\n";
+
+    char buf[1024] = {0,};
+    TcpSocket client = server.Accept();
+
+    while(1){
+        if(!client.IsValid()) continue;
+
+        std::memset(buf, 0, sizeof(buf));
+        ssize_t len = client.Recv(buf, sizeof(buf) - 1);
+        
+
+        if(len>0){
+            buf[len] = '\n';
+            std::cout << "client: " << buf << '\n';
+
+            client.Send(buf, len);
+        } else if(len == 0){
+            std::cout << "client disconnect\n";
+            break;
+        } else {
+            std::cerr << "recv err\n";
+            break;
+        }
+    }
+    return 0;
+}
